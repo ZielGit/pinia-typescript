dbb6a593e341229d4ad077c8df30668bc4c65d42
Frans <frans_421_12@hotmail.com>
2023-08-17 22:08:14 -0500

    feat: Counter store

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,10 +3,12 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const numberOfChanges = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+    numberOfChanges.value++
   }
 
-  return { count, doubleCount, increment }
+  return { count, numberOfChanges , doubleCount, increment }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore();
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <h1>Home View: {{ counterStore.count }}</h1>
+    <h3>Números de cambios: {{ counterStore.numberOfChanges }}</h3>
   </main>
 </template>
